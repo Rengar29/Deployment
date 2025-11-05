a6b860bead9cf74a7d565279513ca7adbfb82c04
Rengar29 <montefalconemman29@gmail.com>
2025-11-06 04:40:47 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js - Compat version
+const firebaseConfig = {
+  apiKey: "AIzaSyCd40unHWbAJCf5Wl4mEcLahMjKWegI_S4",
+  authDomain: "admin-f288c.firebaseapp.com",
+  projectId: "admin-f288c",
+  storageBucket: "admin-f288c.firebasestorage.app",
+  messagingSenderId: "1021415286987",
+  appId: "1:1021415286987:web:88068ad3daf4e5d286d34e",
+  measurementId: "G-SV6M9KVVS1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log('Firebase initialized successfully');
